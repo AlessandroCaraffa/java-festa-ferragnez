77de776c37553679780c35ea32ff1252108097ae
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-19 17:33:31 +0100

    bonus corretto

--- src/com/ferragnez/CheckGuest.java
+++ src/com/ferragnez/CheckGuest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class CheckGuest {
 
 	public static void main(String[] args) {
-		String [] listaInvitati = {"Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Bebe Vio","Luis","Pardi Zarei","Martina Maccherone","Rachel Zeilic"};
+		String [] listaInvitati = {"Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Bebe Vio","Luis","Pardis Zarei","Martina Maccherone","Rachel Zeilic"};
 		Scanner scanner1 = new Scanner(System.in);
 		
 		for (int i= 0; i < listaInvitati.length;i++) {
@@ -16,11 +16,14 @@ public class CheckGuest {
 		}
 		
 		System.out.println("Inserite il vostro nome per l'identificazione: ");
-		String guestName = scanner1.nextLine();
+		String guestName = scanner1.nextLine(); 
 		System.out.println("Nome inserito: " + guestName);
 		
-		boolean nomeCorretto = false;
 		
+		boolean nomeCorretto = false;
+
+		
+//		metodo 1
 //		for (int i=0 ;i< listaInvitati.length ;i++) {
 //			if (listaInvitati[i].equals(guestName)) {
 //				nomeCorretto = true;
@@ -28,13 +31,19 @@ public class CheckGuest {
 //			}
 //		}
 		
+		
+		
+		
+//		metodo2
 		int i = 0;
-		while (i < listaInvitati.length -1) {
-			i++;
+		while (i < listaInvitati.length) {
+			
 			if (listaInvitati[i].equals(guestName)) {
 				nomeCorretto = true;
+				
 				break;
 			}
+			i++;
 		}
 		
 		
